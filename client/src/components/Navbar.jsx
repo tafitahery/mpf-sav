284d84cb08e9383e422773aa7c7e17a141979f41
Tafita Hery <tafitahery@gmail.com>
2022-12-14 16:24:20 +0300

    Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,43 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 50px;
+  background-color: black;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+`;
+
+const Logo = styled.span`
+  font-size: 32px;
+  font-weight: 800;
+`;
+
+const Hamburger = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  height: 50%;
+  cursor: pointer;
+`;
+
+const Bare = styled.span`
+  width: 30px;
+  height: 3px;
+  background-color: white;
+`;
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <Container>
+      <Logo>.mfpSAV</Logo>
+      <Hamburger>
+        <Bare></Bare>
+        <Bare></Bare>
+        <Bare></Bare>
+      </Hamburger>
+    </Container>
+  );
 }
